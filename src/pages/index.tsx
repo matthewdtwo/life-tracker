f3c91bdbd04c0e291c35682e7c313e89b41120d6
Matthew Little <matthew@matthewtwo.com>
2024-02-11 20:55:51 -0600

    added commander tax

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,9 @@ export default function Home() {
   const [topCommanderDamage, setTopCommanderDamage] = useState(0);
   const [bottomCommanderDamage, setBottomCommanderDamage] = useState(0);
 
+  const [topCommanderTax, setTopCommanderTax] = useState(0);
+  const [bottomCommanderTax, setBottomCommanderTax] = useState(0);
+
   const handleTopClick = (increment: boolean) => {
     setTopNumber((prev) => prev + (increment ? 1 : -1));
   }
@@ -28,6 +31,14 @@ export default function Home() {
     setBottomCommanderDamage((prev) => prev + (increment ? 1 : -1));
   }
 
+  const handleTopCommanderTax = (increment: boolean) => {
+    setTopCommanderTax((prev) => prev + (increment ? 2 : -2));
+  }
+
+  const handleBottomCommanderTax = (increment: boolean) => {
+    setBottomCommanderTax((prev) => prev + (increment ? 2 : -2));
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.section} style={{ transform: 'rotate(180deg)' }}>
@@ -41,6 +52,13 @@ export default function Home() {
         <div className={styles.leftSide} onClick={() => handleTopClick(false)}></div>
         <div className={styles.number}>{topNumber}</div>
         <div className={styles.rightSide} onClick={() => handleTopClick(true)}></div>
+
+        <div className={styles.commanderTaxSection}>
+          <div className={styles.commanderTaxLeft} onClick={() => handleTopCommanderTax(false)}></div>
+          <div className={styles.commanderTax}>{topCommanderTax}</div>
+          <div className={styles.commanderTaxRight} onClick={() => handleTopCommanderTax(true)}></div>
+        </div>
+
       </div>
       
       
@@ -57,7 +75,16 @@ export default function Home() {
         <div className={styles.leftSide} onClick={() => handleBottomClick(false)}></div>
         <div className={styles.number}>{bottomNumber}</div>
         <div className={styles.rightSide} onClick={() => handleBottomClick(true)}></div>
+
+        <div className={styles.commanderTaxSection}>
+          <div className={styles.commanderTaxLeft} onClick={() => handleBottomCommanderTax(false)}></div>
+          <div className={styles.commanderTax}>{bottomCommanderTax}</div>
+          <div className={styles.commanderTaxRight} onClick={() => handleBottomCommanderTax(true)}></div>
+        </div>
+
+
       </div>
+
     </div>
   );
 }
