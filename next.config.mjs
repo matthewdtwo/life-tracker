746d343cd875e76eff28f5b89d0bd087e7af99a5
Matthew Little <matthew@matthewtwo.com>
2024-02-11 19:38:31 -0600

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
-  distDir: "docs",
-  // basePath: "/life-tracker",
+  output: "export",
+  // distDir: "docs",
+  basePath: "/life-tracker",
   images: {
     unoptimized: true,
   }
