5a2ec3a613cacb414bc5b0b8c83af25d6cad67f5
Matthew Little <matthew@matthewtwo.com>
2024-02-17 17:01:48 -0600

    fixed spacing

---
+++ docs/_next/static/jQCp2efAuRTzWIpwhTsil/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/86931f33a332854c.css","static/chunks/pages/index-61f5cee8910b933c.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
