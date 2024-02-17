9d377794796ec58cc0e07471438761ea850db2b5
Matthew Little <matthew@matthewtwo.com>
2024-02-17 09:30:08 -0600

    added code to keep the screen awake

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
 import styles from '../styles/Main.module.css';
 import { Tracker } from '@/components/Tracker/Tracker';
 
 export default function Home() {
 
+  useEffect(() => {
+    navigator.wakeLock.request('screen').then(() => {
+      console.log('Screen is locked');
+    }).catch((err) => {
+      console.log('Error: ', err);
+    });
+  }, []);
+  
   return (
     <div className={styles.container}>
         <Tracker inverted={true} />
